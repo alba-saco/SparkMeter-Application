0a074532b923f0700af47810829925e904b9cab4
Alba Saco <56872623+alba-saco@users.noreply.github.com>
2021-01-25 19:48:16 +0000

    Add files via upload

---
+++ fix_the_mistakes.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+# import os
+import os
+# import datetime
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+SLEEP_DURATION = os.getenv("SLEEP_DURATION")
+
+
+class Pipeline:
+    # removed async keyword from __init__ function
+    # added self call to both functions
+    # removed *args parameter since it wasn't used
+    def __init__(self, **kwargs):
+        default_sleep_duration = kwargs["default_sleep_duration"]
+
+    async def sleep_for(self, coro, sleep_duration, *args, **kwargs):
+        # added await keyword to sleep
+        await asyncio.sleep(sleep_duration)
+        # removed indent
+        logger.info("Slept for %s seconds", sleep_duration)
+        start = datetime.now()
+        # changed kwarg to kwargs
+        await coro(*args, **kwargs)
+        end = datetime.now()
+        time_elapsed = (start - end).total_seconds()
+        logger.debug(f"Executed the coroutine for {time_elapsed} seconds")
